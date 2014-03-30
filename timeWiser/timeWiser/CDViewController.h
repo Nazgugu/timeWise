65c4bd14e786d6c531662ac04fa0e5f21bc06dc0
Nazgugu <prada520farrell@gmail.com>
2014-03-30 14:44:28 -0700

    initial project created

---
+++ timeWiser/timeWiser/CDViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDViewController.h
+//  timeWiser
+//
+//  Created by Liu Zhe on 3/30/14.
+//  Copyright (c) 2014 CDFLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDViewController : UIViewController
+
+@end
---
+++ timeWiser/timeWiserTests/timeWiserTests.m
@@ -0,0 +1,34 @@
+//
+//  timeWiserTests.m
+//  timeWiserTests
+//
+//  Created by Liu Zhe on 3/30/14.
+//  Copyright (c) 2014 CDFLS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface timeWiserTests : XCTestCase
+
+@end
+
+@implementation timeWiserTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
