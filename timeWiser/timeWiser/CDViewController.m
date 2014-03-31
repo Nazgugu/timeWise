eb2a4a0ee61eb3200e626ab7f6bf0f3242107c9f
Nazgugu <prada520farrell@gmail.com>
2014-03-30 18:53:59 -0700

    added segment control

--- timeWiser/timeWiser/CDViewController.m
+++ timeWiser/timeWiser/CDViewController.m
@@ -7,17 +7,55 @@
 //
 
 #import "CDViewController.h"
+#import "HMSegmentedControl.h"
+
+#define allowAppearance NO
 
 @interface CDViewController ()
-
+//@property (nonatomic, strong) NSArray *menuItems;
 @end
 
 @implementation CDViewController
 
+
+- (void)loadView
+{
+    [super loadView];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:nil/*@selector(add)*/];
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    HMSegmentedControl *topControl = [[HMSegmentedControl alloc] initWithFrame:CGRectMake(0, self.navigationController.navigationBar.frame.size.height + 20, 320, 45)];
+    topControl.sectionTitles = @[@"TODAY", @"TIMER", @"TASKS"];
+    topControl.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleWidth;
+    topControl.selectedSegmentIndex = 1;
+    topControl.backgroundColor = [UIColor whiteColor];
+    topControl.textColor = [UIColor blackColor];
+    topControl.selectedTextColor = [UIColor blueColor];
+    topControl.selectionIndicatorColor = [UIColor whiteColor];
+    topControl.selectionStyle = HMSegmentedControlSelectionStyleArrow;
+    topControl.selectionIndicatorLocation = HMSegmentedControlSelectionIndicatorLocationDown;
+    topControl.selectionIndicatorColor = [UIColor blueColor];
+    [self.view addSubview:topControl];
+}
+
+- (void)segmentedControlChangedValue:(HMSegmentedControl *)segmentedControl {
+	NSLog(@"Selected index %ld (via UIControlEventValueChanged)", segmentedControl.selectedSegmentIndex);
+}
+
+- (void)uisegmentedControlChangedValue:(UISegmentedControl *)segmentedControl {
+	NSLog(@"Selected index %ld", segmentedControl.selectedSegmentIndex);
+}
+
+
+#pragma mark - UIBarPositioningDelegate Methods
+
+- (UIBarPosition)positionForBar:(id <UIBarPositioning>)view
+{
+    return UIBarPositionBottom;
 }
 
 - (void)didReceiveMemoryWarning
