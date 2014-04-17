037e583065ac1502c4bc02f857b876eef250bb5e
Nazgugu <prada520farrell@gmail.com>
2014-04-17 10:12:48 -0700

    minor bug fix

--- timeWiser/timeWiser/SFCounterLabel.m
+++ timeWiser/timeWiser/SFCounterLabel.m
@@ -177,7 +177,11 @@
     unsigned long long milliSecs = (unsigned long long)(elapsedTime * 1000);
     
     if (self.countDirection == kCountDirectionDown) {
-        [self setValue:(_startValue - milliSecs)];
+        if (_startValue < milliSecs) {
+            [self setValue:0];
+        } else {
+            [self setValue:(_startValue - milliSecs)];
+        }
     } else {
         [self setValue:(_startValue + milliSecs)];
     }
